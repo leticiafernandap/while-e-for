cc4706b92699ebebea0e2c014751753c4a8f3530
LETICIA FERNANDA PEREIRA <leticia.6978@aluno.pr.senac.br>
2023-03-10 08:18:24 -0300

    while

---
+++ par.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Números Pares de 0 a 500</title>
+</head>
+<body>
+    <h1> Números pares de 0 a 500 </h1>
+    <?php
+    for ($par=0; $par<=500; $par++){
+        if (($par % 2 == 1) and ($par < 500)){
+            continue;
+        }elseif ($par <=500) {
+            echo $par. " - ";
+        }else{
+            break;
+        }
+    
+        }
+    
+    ?>
+    
+</body>
+</html>
